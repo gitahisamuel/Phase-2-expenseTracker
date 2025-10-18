c6c3b7af5ea78630f11000499907ade96a46c9ed
gitahisamuel <murimisamuel05@gmail.com>
2025-10-18 09:54:16 +0300

    formfunctionality

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,19 +1,50 @@
 import React from "react";
+import { useState } from "react";
+
 
 
 function Form() {
+
+  const[formData, setFormData]= useState ({
+    expenseName: "",
+    expenseDescription:"",
+    amount:""
+
+
+
+});
+
+const handleChange = (e) => {
+  const { name, value } = e.target;
+
+  setFormData((prevData) => ({
+    ...prevData,
+    [name]: value
+  }));
+};
+
+
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+
+  console.log(formData);
+
+
+};
+
   return (
     <div className="form-container">
       <h2>Add Expense</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label>Expense Name:</label>
-          <input type="text" name="expenseName" />
+          <input type="text" name="expenseName" value={formData.expenseName} onChange={handleChange}/>
         </div>
 
         <div className="form-group">
           <label>Expense Description:</label>
-          <input type="text" name="expenseDescription" />
+          <input type="text" name="expenseDescription" value={formData.expenseName}onChange={handleChange}/>
         </div>
 
         <div className="form-group">
@@ -21,7 +52,7 @@ function Form() {
           <input type="text" name="amount" />
         </div>
 
-        <button type="submit">Submit</button>
+        <button type="submit" >Submit</button>
       </form>
     </div>
   );
